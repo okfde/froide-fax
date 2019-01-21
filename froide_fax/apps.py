6656859f54f787f2364ebefea712b2757de510e6
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-21 14:49:44 +0100

    Add user export callback

--- froide_fax/apps.py
+++ froide_fax/apps.py
@@ -1,4 +1,5 @@
-# -*- encoding: utf-8 -*-
+import json
+
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -9,12 +10,15 @@ class FroideFaxConfig(AppConfig):
 
     def ready(self):
         from froide.account import account_canceled
+        from froide.account.export import registry
         from froide.foirequest.models import FoiRequest
+
         from .listeners import connect_message_send
 
         FoiRequest.message_sent.connect(connect_message_send)
 
         account_canceled.connect(cancel_user)
+        registry.register(export_user_data)
 
 
 def cancel_user(sender, user=None, **kwargs):
@@ -29,3 +33,19 @@ def cancel_user(sender, user=None, **kwargs):
         return
     sig.remove_signature_file()
     sig.delete()
+
+
+def export_user_data(user):
+    from .models import Signature
+    try:
+        signature = Signature.objects.get(user=user)
+    except Signature.DoesNotExist:
+        return
+
+    yield ('signature.json', json.dumps({
+            'timestamp': signature.timestamp.isoformat(),
+        }).encode('utf-8')
+    )
+    signature_bytes = signature.get_signature_bytes()
+    if signature_bytes is not None:
+        yield ('signature.png', signature_bytes)
