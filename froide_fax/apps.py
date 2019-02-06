35f27834a4af72f35fc26c7d84311d1af52cca7c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-02-06 13:35:00 +0100

    Add merge account signal handler

--- froide_fax/apps.py
+++ froide_fax/apps.py
@@ -9,7 +9,7 @@ class FroideFaxConfig(AppConfig):
     verbose_name = _("Froide Fax App")
 
     def ready(self):
-        from froide.account import account_canceled
+        from froide.account import account_canceled, account_merged
         from froide.account.export import registry
         from froide.foirequest.models import FoiRequest
 
@@ -18,6 +18,7 @@ class FroideFaxConfig(AppConfig):
         FoiRequest.message_sent.connect(connect_message_send)
 
         account_canceled.connect(cancel_user)
+        account_merged.connect(merge_user)
         registry.register(export_user_data)
 
 
@@ -33,6 +34,20 @@ def cancel_user(sender, user=None, **kwargs):
         return
     sig.remove_signature_file()
     sig.delete()
+
+
+def merge_user(sender, old_user=None, new_user=None, **kwargs):
+    from .models import Signature
+
+    old_exists = Signature.objects.filter(user=old_user).exists()
+    new_exists = Signature.objects.filter(user=new_user).exists()
+
+    if old_exists and new_exists:
+        old_signature = Signature.objects.get(user=old_user)
+        old_signature.remove_signature_file()
+        old_signature.delete()
+    elif old_exists:
+        Signature.objects.filter(user=old_user).update(user=new_user)
 
 
 def export_user_data(user):
