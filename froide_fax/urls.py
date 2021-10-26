d6a60a1db2379206d2b96ea5fe9a68b5186dafaa
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-10-26 22:43:03 +0200

    Add fax-callback URL without signature

--- froide_fax/urls.py
+++ froide_fax/urls.py
@@ -1,20 +1,26 @@
-from django.conf.urls import url
+from django.conf.urls import path, re_path
 
 from .views import (
-    fax_media_url, fax_status_callback,
-    UpdateSignatureView, send_as_fax,
-    pdf_report
+    fax_media_url,
+    fax_status_callback,
+    UpdateSignatureView,
+    send_as_fax,
+    pdf_report,
 )
 
 urlpatterns = [
-    url(r"^signature/$", UpdateSignatureView.as_view(),
-        name="froide_fax-update_signature"),
-    url(r"^send-fax/(?P<message_id>\d+)/$", send_as_fax,
-        name="froide_fax-send_as_fax"),
-    url(r"^report/(?P<message_id>\d+)/$", pdf_report,
-        name="froide_fax-report"),
-    url(r"^fax-callback/(?P<signed>[^/]+)/$", fax_status_callback,
-        name="froide_fax-status_callback"),
-    url(r"^fax-media/(?P<signed>[^/]+)/$", fax_media_url,
-        name="froide_fax-media_url"),
+    path(
+        "signature/", UpdateSignatureView.as_view(), name="froide_fax-update_signature"
+    ),
+    path("send-fax/<int:message_id>/", send_as_fax, name="froide_fax-send_as_fax"),
+    path("report/<int:message_id>/", pdf_report, name="froide_fax-report"),
+    path("fax-callback/", fax_status_callback, name="froide_fax-status_callback"),
+    re_path(
+        r"^fax-callback/(?P<signed>[^/]+)/$",
+        fax_status_callback,
+        name="froide_fax-status_callback",
+    ),
+    re_path(
+        r"^fax-media/(?P<signed>[^/]+)/$", fax_media_url, name="froide_fax-media_url"
+    ),
 ]
