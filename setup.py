ac8f68286a865b890e6da9fa1de5c8a86c7babc6
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-11-25 20:19:42 +0100

    Add requests dependency

--- setup.py
+++ setup.py
@@ -11,14 +11,13 @@ from setuptools import setup, find_packages
 
 def read(*parts):
     filename = os.path.join(os.path.dirname(__file__), *parts)
-    with codecs.open(filename, encoding='utf-8') as fp:
+    with codecs.open(filename, encoding="utf-8") as fp:
         return fp.read()
 
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
@@ -27,29 +26,24 @@ def find_version(*file_paths):
 setup(
     name="froide_fax",
     version=find_version("froide_fax", "__init__.py"),
-    url='https://github.com/okfde/froide-fax',
-    license='MIT',
+    url="https://github.com/okfde/froide-fax",
+    license="MIT",
     description="Froide fax app",
-    long_description=read('README.md'),
-    author='Stefan Wehrmeyer',
-    author_email='mail@stefanwehrmeyer.com',
+    long_description=read("README.md"),
+    author="Stefan Wehrmeyer",
+    author_email="mail@stefanwehrmeyer.com",
     packages=find_packages(),
-    install_requires=[
-        'froide',
-        'weasyprint',
-        'phonenumbers',
-        'pynacl'
-    ],
+    install_requires=["froide", "weasyprint", "phonenumbers", "pynacl", "requests"],
     include_package_data=True,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Utilities",
     ],
     zip_safe=False,
 )
